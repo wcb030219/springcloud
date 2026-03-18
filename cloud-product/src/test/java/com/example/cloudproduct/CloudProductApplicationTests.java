e989db6c9a3d84c17f92f9e342a0ac8306b72bbb
wcb030219 <3056233763@qq.com>
2026-03-18 13:45:29 +0800

    first

---
+++ cloud-product/src/test/java/com/example/cloudproduct/CloudProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
